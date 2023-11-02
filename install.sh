5b06741c4d92cfa2ed7b15351a8164834c2784ee
n0ns0n <n0ns0n>
2023-11-02 23:16:32 +0200

    echo messages for install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-#Installing Requirements.
+CRESET="\e[0m"
+RED="\e[1;31m"
+CYAN="\e[1;36m"
+echo -e "${CYAN}[+] Installing python3 requirements${CRESET}"
 sudo apt install python3-{requests,tk,bs4,tarfile,json}
-
 Check_Discord () {
-#Wanna do it yourself or we can do it for ya?	
+	#Wanna do it yourself or we can do it for ya?
 	app_name="discord"
 	dpkg -l | grep $app_name &>/dev/null
 	if [ $? -eq 0 ]; then
-	    echo "found deb"
+        echo -e "\n\n${RED}[+] Discord is installed${CRESET}"
+        echo -e "${RED}[+] Trying to uninstall first${CRESET}\n\n"
 	    sudo apt-get remove $app_name
 	else
 	    snap list | grep $app_name &> /dev/null
 	    if [ $? -eq 0 ]; then
-	        echo "found snap"
+	        echo -e "\n\n${RED}[+] Discord is installed as a snap${CRESET}"
+	        echo -e "${RED}[+] Trying to uninstall first${CRESET}\n\n"
 	        sudo snap remove $app_name
 	    fi
 	fi
 }
-#Checking Discorc is Installed
+echo -e "${CYAN}[+] Checking if Discord is installed${CRESET}"
 Check_Discord       
-#Creatig synlink.          
+echo -e "${CYAN}[+] Creating symlink to /usr/local/bin/discord${CRESET}"       
 sudo ln -sf $(pwd)/discord.py /usr/local/bin/discord
-#Creating .Desktop File.
+echo -e "${CYAN}[+] Creating .desktop file${CRESET}"
 desktop_file="[Desktop Entry]
 Name=Discord
 StartupWMClass=discord
@@ -31,7 +35,4 @@ Exec=/usr/local/bin/discord
 Icon=$HOME/.local/share/Discord/discord.png
 Type=Application
 Categories=Network;InstantMessaging;"
-
 echo "$desktop_file" | sudo tee /usr/share/applications/discord.desktop
-
-
